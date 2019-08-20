ea28eaabc8e104b514e9901372ee11eba23c277e
diego <diego@MacBook-Pro-de-soporte.local>
2019-08-20 17:13:12 -0300

    Add navigation

---
+++ app/src/main/java/katas/client/twitter/ui/fragments/TweetsFragment.kt
@@ -0,0 +1,23 @@
+package katas.client.twitter.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import katas.client.twitter.R
+
+object TweetsViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+class TweetsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tweets, container, false)
+    }
+}
